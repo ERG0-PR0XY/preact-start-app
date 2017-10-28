1a8e8c4c5a5b23378f449f7ee62d0ce8f2b972ce
jerin.constant <scarlatkn8@gmail.com>
2017-10-28 19:38:38 +0530

    Revert "Initial commit"
    This reverts commit 5011a2c6a4cdc5072322ee294a5d4351e3932e93.

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { h, render } from 'preact'
-import App from './components/App'
-
-document.addEventListener('DOMContentLoaded', event => (
-    render(<App />, document.getElementById('app'))
-))
